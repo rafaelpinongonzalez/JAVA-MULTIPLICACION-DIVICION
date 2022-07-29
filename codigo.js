e305b1d45c11072e80ddef3fb59812763fcaee3f
rafael piñón gonzalez <rafapinongonzalez@live.com.mx>
2022-07-29 13:33:54 -0600

    javamultiplicarydividir

---
+++ codigo.js
@@ -0,0 +1,42 @@
+// var numerouno = 50;
+// var nuemrodos = 20;
+//  document.write (numerouno == nuemrodos)
+
+
+var dompesarenados = 2
+
+var precio= 1200
+ 
+ 
+
+document.write (dompesarenados * precio) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ASI ES COMO SE ESCRIBEN EL TIPO DE DATOS QUE QUIERAS PPONER 
+// YA SEA TEXTOS O NUMEROS  O PPNER CONDICIONES FALSO O VERDADERO
+// String = "cadena de textis"
+// number = 19
+// booleano = false o true
